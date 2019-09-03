b4040439b540d29f8dd1889e75f567580903e3e7
tchaves <tchaves@a5solutions.com>
2019-09-03 16:28:19 -0300

    Criacao de Persistencia JPA e findByNomeCurso

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,33 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+	@Autowired
+	private TopicoRepository repository;
 
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista(){
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring","Programação"));
-	
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	public List<TopicoDto> lista(String nomeCurso){
+		
+		if(nomeCurso == null ) {
+			List<Topico> topicos = repository.findAll();
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = repository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+		
+		
+		
 	
 	}
 }
