3e3d8ae7d115400d5b94f3c986e2422ca9a5af65
tchaves <tchaves@a5solutions.com>
2019-09-03 20:02:59 -0300

    Classe ErroDeValidacaoHandler

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,14 +1,21 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.controller.repository.CursoRepository;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
 public class TopicoForm {
 
-	
+	@NotNull @NotEmpty @Length(min = 5)
 	private String titulo;
+	@NotNull @NotEmpty @Length(min = 5)
 	private String mensagem;
+	@NotNull @NotEmpty 
 	private String nomeCurso;
 	
 	
